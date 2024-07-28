b3acecee7ec5632041f840bec4a4e537694e4854
pawelek <k.pawelek@vp.pl>
2024-07-28 17:12:25 +0200

    add hash paswd

--- pages/api/auth/singup.ts
+++ pages/api/auth/singup.ts
@@ -1,13 +1,21 @@
 import { connectToDatabase } from "@/lib/db"
+import { schema } from "@/lib/schemaJoi"
 import { NextApiRequest, NextApiResponse } from "next"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const client = await connectToDatabase()
     const db = client.db()
 
-    db.collection('users')
     const data = req.body
+
     const { email, name, password } = data
+    const { error, value } = schema.validate({ email, name, password })
+
+    if (error) {
+        return res.status(400).json({ error: error.message })
+    }
+
+    const result = await db.collection('users').insertOne(value)
 }
 
 
