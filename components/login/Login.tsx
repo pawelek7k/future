db7ba356bada7cf990c3f903053d0b3fa7143b94
pawelek <k.pawelek@vp.pl>
2024-07-28 16:08:16 +0200

    add files & mongodb

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -5,14 +5,14 @@ export const LoginForm = () => {
       <form>
         <div className="mb-4">
           <label
-            htmlFor="email"
+            htmlFor="email2"
             className="block text-gray-700 text-sm font-medium mb-2"
           >
             E-mail
           </label>
           <input
             type="email"
-            id="email"
+            id="email2"
             name="email"
             placeholder="Wpisz swój e-mail"
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
@@ -20,14 +20,14 @@ export const LoginForm = () => {
         </div>
         <div className="mb-4">
           <label
-            htmlFor="password"
+            htmlFor="password2"
             className="block text-gray-700 text-sm font-medium mb-2"
           >
             Hasło
           </label>
           <input
             type="password"
-            id="password"
+            id="password2"
             name="password"
             placeholder="Wpisz swoje hasło"
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
