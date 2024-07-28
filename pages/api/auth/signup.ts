254982c50a4435fc57e3f3f1d0d1e2b3bf90f105
pawelek <k.pawelek@vp.pl>
2024-07-28 18:41:03 +0200

    Update signup.ts

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -4,37 +4,35 @@ import { schema } from "@/lib/schemaJoi";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
-        try {
-            const client = await connectToDatabase();
-            const db = client.db();
-            const data = req.body;
-            const { email, username, password } = data;
-
-            const { error, value } = schema.validate({ email, name: username, password });
-
-            if (error) {
-                return res.status(400).json({ error: error.message });
-            }
-
-            const hashedPassword = await hashPassword(password);
-
-            const result = await db.collection('users').insertOne({
-                ...value,
-                password: hashedPassword
-            });
-
-            return res.status(201).json({ message: 'User created successfully', userId: result.insertedId });
-
-        } catch (e) {
-            console.error('Database operation failed:', e);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-
-    } else {
+    if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
+    try {
+        const client = await connectToDatabase();
+        const db = client.db();
+        const data = req.body;
+        const { email, username, password } = data;
+
+        const { error, value } = schema.validate({ email, name: username, password });
+
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db.collection('users').insertOne({
+            ...value,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({ message: 'User created successfully', userId: result.insertedId });
+
+    } catch (e) {
+        console.error('Database operation failed:', e);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 };
 
 export default handler;
