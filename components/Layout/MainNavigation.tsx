307b9b74e78c61605e3ad2098d46bc1ea30d1b22
pawelek <k.pawelek@vp.pl>
2024-07-29 19:00:17 +0200

    Update MainNavigation.tsx

--- components/Layout/MainNavigation.tsx
+++ components/Layout/MainNavigation.tsx
@@ -11,12 +11,21 @@ export const MainNavigation = () => {
     <header className="flex justify-evenly p-4 w-full text-text fixed z-50 bg-secondaryBg backdrop-blur-md rounded-b-lg border-b border-secondary top-0">
       <nav>
         <ul className="flex gap-8 tracking-widest">
-          <li>
-            <Link href="/login">Join us</Link>
-          </li>
-          <li>
-            <Link href="/profile">Your profile</Link>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/login">Join us</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Your profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
