758d4166ee9e6b1e625fb7be53977f036c1650c9
pawelek <k.pawelek@vp.pl>
2024-07-30 14:33:13 +0200

    add change password logic

--- pages/api/user/changePassword.ts
+++ pages/api/user/changePassword.ts
@@ -1,3 +1,5 @@
+import { connectToDatabase } from '@/lib/db';
+import { hashPassword, verifyPassword } from '@/lib/signup/auth';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 
@@ -17,6 +19,36 @@ export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         const userEmail = session.user?.email
         const oldPassword = req.body.oldPassword
         const newPassword = req.body.newPassword
+
+        const client = await connectToDatabase()
+        const usersCollection = client.db().collection('users')
+
+        const user = await usersCollection.findOne({ email: userEmail })
+
+        if (!user) {
+            res.status(404).json({ message: 'User not found' })
+            client.close()
+            return
+        }
+
+        const currentPassword = user.password
+
+        const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword)
+
+        if (!passwordsAreEqual) {
+            res.status(422).json({ message: 'Invalid password.' })
+            client.close()
+            return
+        }
+
+        const hashedPassword = await hashPassword(newPassword)
+
+        const result = await usersCollection.updateOne(
+            { email: userEmail },
+            { $set: { password: newPassword } })
+
+        client.close()
+        res.status(200).json({ message: 'Password updated!' })
 
     } catch (error) { }
 };
