5606cb5bd51065fd7df94f440aac4eea919cfe30
pawelek <k.pawelek@vp.pl>
2024-07-30 14:18:16 +0200

    Update changePassword.ts

--- pages/api/user/changePassword.ts
+++ pages/api/user/changePassword.ts
@@ -1,7 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'PATCH') {
+        return
+    }
+
+    const session = await getSession({ req })
+
+    if (!session) {
+        res.status(401).json({ message: 'Not authenticated!' })
         return
     }
 };
