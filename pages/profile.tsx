2a5cd24b02384399de0922f5d687e20ce325566a
pawelek <k.pawelek@vp.pl>
2024-07-30 13:42:02 +0200

    fixes

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,25 +1,40 @@
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const UserProfilePage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [loadedSession, setLoadedSession] = useState();
+  const [session, setSession] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/login";
-      } else {
-        setIsLoading(false);
+    const fetchSession = async () => {
+      try {
+        const session = await getSession();
+        if (!session) {
+          router.push("/login");
+        } else {
+          setSession(session);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error("Failed to fetch session", error);
       }
-    });
-  }, []);
+    };
+
+    fetchSession();
+  }, [router]);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  return <h1>ok</h1>;
+  return (
+    <div className="mt-20">
+      <h1>User Profile</h1>
+      <p>Welcome, {session.user.username}!</p>
+    </div>
+  );
 };
 
 export default UserProfilePage;
