e7007486b6f165cf28cab39c0adff5274d40e0fb
pawelek <k.pawelek@vp.pl>
2024-07-30 13:39:42 +0200

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,24 @@
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
 const UserProfilePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/login";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return <h1>ok</h1>;
 };
 
