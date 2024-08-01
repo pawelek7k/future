eae0e3711e8712587cdd7cc7b379c0e666dcd957
pawelek <k.pawelek@vp.pl>
2024-08-01 14:50:39 +0200

    Update ImagePicker.tsx

--- components/settings/Profile/ImagePicker.tsx
+++ components/settings/Profile/ImagePicker.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ChangeEvent, useRef, useState } from "react";
 
 export const ImagePicker = () => {
@@ -21,5 +22,38 @@ export const ImagePicker = () => {
     };
     fileReader.readAsDataURL(file);
   };
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      <label></label>
+      <div className="flex flex-col gap-4 w-[15rem]">
+        <div className="w-40 h-40 rounded-full relative border border-myPrimary flex items-center justify-center font-sans">
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              layout="fill"
+              objectFit="cover"
+            />
+          )}
+        </div>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          ref={imageInput}
+          className="hidden"
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          type="button"
+          className="bg-accent text-text"
+          onClick={handlePickClick}
+        >
+          Choose a cover
+        </button>
+      </div>
+    </div>
+  );
 };
