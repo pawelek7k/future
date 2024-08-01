812b65195cbff067163f33aa9c6d164fab459df2
pawelek <k.pawelek@vp.pl>
2024-08-01 18:17:52 +0200

    add more content to notification container

--- components/settings/Notification/index.tsx
+++ components/settings/Notification/index.tsx
@@ -1,13 +1,19 @@
 import { ThirdHeading } from "@/components/global/heading";
 import { Paragraph } from "@/components/global/text";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { SearchInput } from "./SearchInput";
 
 export const NotificationContainer = () => {
   return (
     <div>
-      <ul>
-        <li className="border-b border-gray-200">
+      <SearchInput />
+      <ul className="mt-10">
+        <li className="border-b border-gray-200 w-1/2">
           <ThirdHeading>Powiadomienie</ThirdHeading>
-          <Paragraph>Udało Ci się zalogować.</Paragraph>
+          <div className="flex justify-between">
+            <Paragraph>Udało Ci się zalogować.</Paragraph>
+            <IoTrashBinOutline className="cursor-pointer" />
+          </div>
         </li>
       </ul>
     </div>
