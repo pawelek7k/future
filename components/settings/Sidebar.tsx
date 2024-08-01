baa1460b734bcd7c98300989bd68424a4c7b01e8
pawelek <k.pawelek@vp.pl>
2024-08-01 22:54:26 +0200

    Update Sidebar.tsx

--- components/settings/Sidebar.tsx
+++ components/settings/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { Divide as Hamburger } from "hamburger-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
@@ -23,20 +23,40 @@ const sidebarItems = [
 
 export const Sidebar = () => {
   const [isOpen, setOpen] = useState(true);
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop) {
+      setOpen(true);
+    }
+  }, [isDesktop]);
 
   const toggleSidebar = () => {
-    setOpen(!isOpen);
+    if (!isDesktop) {
+      setOpen(!isOpen);
+    }
   };
 
   return (
     <div>
-      <div className="fixed z-50 left-2 top-2">
+      <div className={`fixed z-50 left-2 top-2 ${isDesktop ? "hidden" : ""}`}>
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
       <div
         className={`fixed top-0 left-0 h-screen bg-neutral-100 text-sky-950 p-4 pt-20 transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "-translate-x-full"
-        } md:relative md:translate-x-0 md:w-1/4`}
+        } ${isDesktop ? "md:relative md:translate-x-0 md:w-1/4" : ""}`}
       >
         <SecondHeading>Ustawienia</SecondHeading>
         <ul className="space-y-2 mt-6">
@@ -45,6 +65,7 @@ export const Sidebar = () => {
               <Link
                 href={`/settings/${item.id}`}
                 className="p-2 hover:bg-neutral-200 rounded flex items-center gap-4"
+                onClick={toggleSidebar}
               >
                 {item.icon}
                 {item.label}
