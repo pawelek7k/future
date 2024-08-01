4749fd020e37d38b838ba0fc83aab10559f0053d
pawelek <k.pawelek@vp.pl>
2024-08-01 14:45:54 +0200

    Update ImagePicker.tsx

--- components/settings/Profile/ImagePicker.tsx
+++ components/settings/Profile/ImagePicker.tsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 export const ImagePicker = () => {
   const [pickedImage, setPickedImage] = useState<string | null>(null);
+  const imageInput = useRef<HTMLInputElement>(null);
+
+  const handlePickClick = () => {
+    imageInput.current?.click();
+  };
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return setPickedImage(null);
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result as string);
+    };
+    fileReader.readAsDataURL(file);
+  };
   return <div></div>;
 };
