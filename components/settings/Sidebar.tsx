4f1b5d798a20dc8a68acfcdce5a0c2b8771da1f1
pawelek <k.pawelek@vp.pl>
2024-08-01 22:34:06 +0200

    add document draft

--- components/settings/Sidebar.tsx
+++ components/settings/Sidebar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MdOutlineManageAccounts, MdSecurity } from "react-icons/md";
 import { RiGitRepositoryPrivateLine } from "react-icons/ri";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { SecondHeading } from "../global/heading";
 
 const sidebarItems = [
@@ -20,22 +22,40 @@ const sidebarItems = [
 ];
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="w-1/4 bg-neutral-100 text-sky-950 h-screen p-4 pt-20">
-      <SecondHeading>Ustawienia</SecondHeading>
-      <ul className="space-y-2 mt-6">
-        {sidebarItems.map((item) => (
-          <li key={item.id}>
-            <Link
-              href={`/settings/${item.id}`}
-              className="p-2 hover:bg-neutral-200 rounded flex items-center gap-4"
-            >
-              {item.icon}
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <button
+        className="mt-20 fixed z-50 -top-16 left-4"
+        onClick={toggleSidebar}
+      >
+        <RxHamburgerMenu className="w-8 h-8" />
+      </button>
+      <div
+        className={`fixed top-0 left-0 h-screen bg-neutral-100 text-sky-950 p-4 pt-20 transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } md:relative md:translate-x-0 md:w-1/4`}
+      >
+        <SecondHeading>Ustawienia</SecondHeading>
+        <ul className="space-y-2 mt-6">
+          {sidebarItems.map((item) => (
+            <li key={item.id}>
+              <Link
+                href={`/settings/${item.id}`}
+                className="p-2 hover:bg-neutral-200 rounded flex items-center gap-4"
+              >
+                {item.icon}
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
