9b4840aca672a00d468233062830ab76842c7b1c
pawelek <k.pawelek@vp.pl>
2024-08-03 23:19:25 +0200

    Update index.tsx

--- components/settings/Profile/index.tsx
+++ components/settings/Profile/index.tsx
@@ -10,9 +10,9 @@ export const ProfileContainer: React.FC = () => {
         <SecondHeading>Profil użytkownika</SecondHeading>
       </div>
       <div className="flex items-center justify-center mt-10 ">
-        <div className="shadow-lg flex flex-col items-center justify-center rounded-lg w-1/3 backdrop-blur-xl bg-white/10 p-6">
+        <div className="shadow-lg flex flex-col items-center justify-center rounded-3xl w-[30rem] backdrop-blur-xl bg-white/10 p-6">
           <ImagePicker />
-          <div className="flex flex-col gap-2 items-center ">
+          <div className="flex flex-col gap-2 items-center -mt-8 bg-white/30 dark:bg-black/60 p-10 rounded-md">
             <ThirdHeading>Nazwa użytkownika</ThirdHeading>
             <Line />
             <div className="flex items-center gap-2">
