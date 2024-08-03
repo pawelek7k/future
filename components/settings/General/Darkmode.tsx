eab179bbcb16daec6fd5bf23ecca3b6eac63beeb
pawelek <k.pawelek@vp.pl>
2024-08-03 19:18:22 +0200

    add styles

--- components/settings/General/Darkmode.tsx
+++ components/settings/General/Darkmode.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { PrimaryButton } from "@/components/global/buttons";
 import { useTheme } from "next-themes";
 
 export const Button = () => {
@@ -6,11 +7,12 @@ export const Button = () => {
   const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
-    <button
-      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-      className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl mt-40"
-    >
-      Toggle Mode
-    </button>
+    <div className="w-40">
+      <PrimaryButton
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      >
+        Toggle Mode
+      </PrimaryButton>
+    </div>
   );
 };
