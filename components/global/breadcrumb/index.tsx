6021f08b2aa5371931c65ffd2d9e546863ee8106
pawelek <k.pawelek@vp.pl>
2024-08-03 22:42:49 +0200

    Update index.tsx

--- components/global/breadcrumb/index.tsx
+++ components/global/breadcrumb/index.tsx
@@ -10,23 +10,21 @@ export const BreadcrumbContainer = () => {
     str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
-    <div>
-      <Breadcrumbs>
-        <BreadcrumbItem key="home">
-          <Link href="/">Home</Link>
+    <Breadcrumbs>
+      <BreadcrumbItem key="home">
+        <Link href="/">Home</Link>
+      </BreadcrumbItem>
+      {segments.map((item, index) => (
+        <BreadcrumbItem key={item}>
+          {index === segments.length - 1 ? (
+            <span>{capitalize(item)}</span>
+          ) : (
+            <Link href={`/${segments.slice(0, index + 1).join("/")}`}>
+              {capitalize(item)}
+            </Link>
+          )}
         </BreadcrumbItem>
-        {segments.map((item, index) => (
-          <BreadcrumbItem key={item}>
-            {index === segments.length - 1 ? (
-              <span>{capitalize(item)}</span>
-            ) : (
-              <Link href={`/${segments.slice(0, index + 1).join("/")}`}>
-                {capitalize(item)}
-              </Link>
-            )}
-          </BreadcrumbItem>
-        ))}
-      </Breadcrumbs>
-    </div>
+      ))}
+    </Breadcrumbs>
   );
 };
