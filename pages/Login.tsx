4574162294a51a27f260ff5e18f4c8e319e65a10
pawelek <k.pawelek@vp.pl>
2024-08-03 19:42:31 +0200

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -20,7 +20,7 @@ export const getServerSideProps = async (
     if (session) {
       return {
         redirect: {
-          destination: "/settings/profile",
+          destination: "/settings",
           permanent: false,
         },
       };
