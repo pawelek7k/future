b18dd93a53c4efcfdd0829047300a831952e3a38
pawelek <k.pawelek@vp.pl>
2024-08-04 22:52:13 +0200

    Update FormContainer.tsx

--- components/create/FormContainer.tsx
+++ components/create/FormContainer.tsx
@@ -41,7 +41,17 @@ export const CreateForm: React.FC = () => {
               className="w-full px-3 py-2 rounded-lg dark:text-neutral-100 dark:bg-rose-950/30  text-gray-900  placeholder-gray-500  focus:outline-none focus:border focus:border-sky-950 dark:focus:border-rose-950 shadow-lg backdrop-blur-md"
             />
           </div>
-          title input title description
+          <label
+            htmlFor="description"
+            className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
+          >
+            Description
+          </label>
+          <textarea
+            id="description"
+            className="w-full px-3 py-2 rounded-lg dark:text-neutral-100 dark:bg-rose-950/30 text-gray-900 placeholder-gray-500 focus:outline-none focus:border focus:border-sky-950 dark:focus:border-rose-950 shadow-lg backdrop-blur-md resize-none"
+            placeholder="Describe your book"
+          />
           <div className="mt-6 flex gap-6 items-center">genre checkbox</div>
           <Tags />
         </div>
