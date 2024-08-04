3bc7add51f02f4d67fe265a1d04724f608e0e9b5
pawelek <k.pawelek@vp.pl>
2024-08-04 15:11:45 +0200

    add styles

--- components/settings/Profile/ImagePicker.tsx
+++ components/settings/Profile/ImagePicker.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, ReactNode, useRef, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+
+interface Props {
+  children: ReactNode;
+}
 
 export const ImagePicker: React.FC = () => {
   const [pickedImage, setPickedImage] = useState<string | null>(null);
@@ -51,6 +55,55 @@ export const ImagePicker: React.FC = () => {
           required
         />
       </div>
+    </div>
+  );
+};
+
+export const BackgroundImagePicker: React.FC<Props> = ({ children }) => {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
+  const imageInput = useRef<HTMLInputElement>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return setPickedImage(null);
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result as string);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  const handleImageClick = () => {
+    imageInput.current?.click();
+  };
+
+  return (
+    <div
+      className="relative w-[30rem] rounded-br-3xl rounded-tl-3xl h-full cursor-pointer"
+      onClick={handleImageClick}
+      style={{
+        backgroundImage: pickedImage ? `url(${pickedImage})` : undefined,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        border: pickedImage ? "none" : "2px dashed #ccc",
+      }}
+    >
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        ref={imageInput}
+        className="hidden"
+        onChange={handleImageChange}
+        required
+      />
+      <div className="absolute inset-0 flex items-center justify-center text-white">
+        {!pickedImage && "Click to select an image"}
+      </div>
+      {children}
     </div>
   );
 };
--- components/settings/Profile/index.tsx
+++ components/settings/Profile/index.tsx
@@ -1,7 +1,7 @@
 import { SecondHeading, ThirdHeading } from "@/components/global/heading";
 import { Line } from "@/components/global/line";
 import { FaRegCopy } from "react-icons/fa";
-import { ImagePicker } from "./ImagePicker";
+import { BackgroundImagePicker, ImagePicker } from "./ImagePicker";
 
 export const ProfileContainer: React.FC = () => {
   return (
@@ -9,21 +9,21 @@ export const ProfileContainer: React.FC = () => {
       <div className="text-center">
         <SecondHeading>Profil użytkownika</SecondHeading>
       </div>
-      <div className="flex items-center justify-center mt-10 ">
-        <div className="shadow-lg flex flex-col rounded-tl-3xl rounded-br-3xl  w-[30rem] backdrop-blur-xl bg-white/10 p-6 relative">
-          {/* <div className="bg-black rounded-tl-3xl w-[30rem] h-24 absolute z-1 top-0"></div> */}
-          <ImagePicker />
-
-          <div className="flex flex-col gap-2 items-center -mt-12 bg-white/30 dark:bg-black/60 p-10 rounded-md pt-14">
-            <ThirdHeading>Nazwa użytkownika</ThirdHeading>
-            <Line />
-            <div className="flex items-center gap-2">
-              <ThirdHeading>Id użytkownika: ID</ThirdHeading>
-              <FaRegCopy className="cursor-pointer" />
+      <div className="flex items-center justify-center mt-10">
+        <BackgroundImagePicker>
+          <div className="shadow-lg flex flex-col rounded-tl-3xl rounded-br-3xl w-[30rem] backdrop-blur-xl bg-white/10 p-6 relative">
+            <ImagePicker />
+            <div className="flex flex-col gap-2 items-center -mt-12  p-10 rounded-md pt-14">
+              <ThirdHeading>Nazwa użytkownika</ThirdHeading>
+              <Line />
+              <div className="flex items-center gap-2">
+                <ThirdHeading>Id użytkownika: ID</ThirdHeading>
+                <FaRegCopy className="cursor-pointer" />
+              </div>
+              <ThirdHeading>Lokalizacja: Warszawa</ThirdHeading>
             </div>
-            <ThirdHeading>Lokalizacja: Warszawa</ThirdHeading>
           </div>
-        </div>
+        </BackgroundImagePicker>
       </div>
     </div>
   );
