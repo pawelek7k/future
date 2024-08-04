517149a41830219fb04e158fb197775a0096313b
pawelek <k.pawelek@vp.pl>
2024-08-04 15:24:32 +0200

    add styled fixes

--- components/settings/Profile/ImagePicker.tsx
+++ components/settings/Profile/ImagePicker.tsx
@@ -1,3 +1,4 @@
+import { SecondHeading } from "@/components/global/heading";
 import Image from "next/image";
 import { ChangeEvent, ReactNode, useRef, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
@@ -30,7 +31,7 @@ export const ImagePicker: React.FC = () => {
 
   return (
     <div className="z-10">
-      <div className="bg-black/30 p-2 rounded-full dark:bg-black/60 backdrop-blur-lg">
+      <div className="bg-white/20 p-2 rounded-full dark:bg-black/60 backdrop-blur-lg flex items-center gap-4">
         <div
           className="w-24 h-24 rounded-full relative border border-sky-950 dark:border-rose-950 flex items-center justify-center font-sans cursor-pointer z-10"
           onClick={handleImageClick}
@@ -54,6 +55,7 @@ export const ImagePicker: React.FC = () => {
           onChange={handleImageChange}
           required
         />
+        <SecondHeading>Nazwa użytkownika</SecondHeading>
       </div>
     </div>
   );
--- components/settings/Profile/index.tsx
+++ components/settings/Profile/index.tsx
@@ -11,16 +11,17 @@ export const ProfileContainer: React.FC = () => {
       </div>
       <div className="flex items-center justify-center mt-10">
         <BackgroundImagePicker>
-          <div className="shadow-lg flex flex-col rounded-tl-3xl rounded-br-3xl w-[30rem] backdrop-blur-xl bg-white/10 p-6 relative">
-            <ImagePicker />
-            <div className="flex flex-col gap-2 items-center -mt-12  p-10 rounded-md pt-14">
-              <ThirdHeading>Nazwa użytkownika</ThirdHeading>
-              <Line />
-              <div className="flex items-center gap-2">
-                <ThirdHeading>Id użytkownika: ID</ThirdHeading>
-                <FaRegCopy className="cursor-pointer" />
+          <div className="shadow-lg flex flex-col rounded-tl-3xl rounded-br-3xl  bg-white/10 p-6 relative">
+            <div className="backdrop-blur-md rounded-t-[3.5rem] rounded-br-3xl bg-white/10">
+              <ImagePicker />
+              <div className="flex flex-col gap-2 items-center -mt-12  p-10 rounded-md pt-14">
+                <Line />
+                <div className="flex items-center gap-2">
+                  <ThirdHeading>Id użytkownika: ID</ThirdHeading>
+                  <FaRegCopy className="cursor-pointer" />
+                </div>
+                <ThirdHeading>Lokalizacja: Warszawa</ThirdHeading>
               </div>
-              <ThirdHeading>Lokalizacja: Warszawa</ThirdHeading>
             </div>
           </div>
         </BackgroundImagePicker>
