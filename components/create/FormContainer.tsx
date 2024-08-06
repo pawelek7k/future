2596aabbd3b4200a40f41c51d3c36432c84442dc
pawelek <k.pawelek@vp.pl>
2024-08-06 18:35:35 +0200

    export func

--- components/create/FormContainer.tsx
+++ components/create/FormContainer.tsx
@@ -1,52 +1,11 @@
 import React from "react";
+import { shareBook } from "../../lib/actions";
 import { CoverPicker } from "./CoverPicker";
 import { DropdownMenu } from "./Dropdown";
 import { Tags } from "./Tags";
 import { ToggleSwitch } from "./ToggleSwitch";
 
-interface BookFormData {
-  title: string;
-  description: string;
-  selectedCover: string;
-  genre: string;
-  checkbox: boolean;
-  tags: string[];
-}
-
 export const CreateForm: React.FC = () => {
-  const shareBook = async (formData: FormData) => {
-    try {
-      const title = formData.get("title") as string;
-      const description = formData.get("description") as string;
-      const selectedCover = formData.get("selectedCover") as string;
-      const genre = formData.get("genre") as string;
-      const checkbox = formData.get("forAdult") === "on";
-      const tagsValue = formData.get("tags");
-
-      let tags: string[] = [];
-      if (tagsValue) {
-        try {
-          tags = JSON.parse(tagsValue as string);
-        } catch (error) {
-          console.error("Error parsing tags JSON:", error);
-        }
-      }
-
-      const book: BookFormData = {
-        title,
-        description,
-        selectedCover,
-        genre,
-        checkbox,
-        tags,
-      };
-
-      console.log(book);
-    } catch (error) {
-      console.error("Error processing form data:", error);
-    }
-  };
-
   return (
     <div className="flex flex-col items-center justify-center mt-10 sm:mt-0">
       <form
---
+++ lib/actions.ts
@@ -0,0 +1,43 @@
+"use server";
+
+interface BookFormData {
+  title: string;
+  description: string;
+  selectedCover: string;
+  genre: string;
+  checkbox: boolean;
+  tags: string[];
+}
+
+export const shareBook = async (formData: FormData) => {
+  try {
+    const title = formData.get("title") as string;
+    const description = formData.get("description") as string;
+    const selectedCover = formData.get("selectedCover") as string;
+    const genre = formData.get("genre") as string;
+    const checkbox = formData.get("forAdult") === "on";
+    const tagsValue = formData.get("tags");
+
+    let tags: string[] = [];
+    if (tagsValue) {
+      try {
+        tags = JSON.parse(tagsValue as string);
+      } catch (error) {
+        console.error("Error parsing tags JSON:", error);
+      }
+    }
+
+    const book: BookFormData = {
+      title,
+      description,
+      selectedCover,
+      genre,
+      checkbox,
+      tags,
+    };
+
+    console.log(book);
+  } catch (error) {
+    console.error("Error processing form data:", error);
+  }
+};
