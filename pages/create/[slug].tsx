739d0bb97f04d28fff28ec6c870f342eae6dc1e0
pawelek <k.pawelek@vp.pl>
2024-08-08 18:16:30 +0200

    add editor

--- pages/create/[slug].tsx
+++ pages/create/[slug].tsx
@@ -1,7 +1,15 @@
-import { FirstHeading } from "@/app/components/global/heading";
 import Head from "next/head";
+import { SetStateAction, useState } from "react";
 
 const CreateChapters: React.FC = () => {
+  const [text, setText] = useState("");
+
+  const handleChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setText(event.target.value);
+  };
+
   return (
     <>
       <Head>
@@ -9,8 +17,14 @@ const CreateChapters: React.FC = () => {
         <meta name="description" content="Posts about programming" />
       </Head>
       <section className="flex flex-col gap-16 mt-8 min-h-screen">
-        <div>
-          <FirstHeading>ok</FirstHeading>
+        <div className="container mx-auto p-4">
+          <div className=" rounded-lg p-4">
+            <textarea
+              className="w-full h-96 p-2 border border-gray-300 rounded-md resize-none focus:outline-none"
+              value={text}
+              onChange={handleChange}
+            />
+          </div>
         </div>
       </section>
     </>
