10fafa2d50e81f5b3cea54bb25a267b96aa0b084
pawelek <k.pawelek@vp.pl>
2024-08-11 15:16:21 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { SocialContainer } from "@/app/components/hero/Socials";
 import { HeroNavigation } from "@/app/components/layout/HeroNavigation";
 import Head from "next/head";
+import Link from "next/link";
 
 const HomePage: React.FC = () => {
   return (
@@ -13,11 +14,24 @@ const HomePage: React.FC = () => {
       <section className="h-screen bg-hero-marketing-bg flex items-center justify-start ">
         <SocialContainer />
         <div className=" w-1/2 h-1/2 flex items-center justify-start backdrop-blur-md rounded-br-3xl rounded-tl-3xl pl-40">
-          <h1 className="sm:text-6xl font-semibold font-sans text-neutral-50 text-xl text-start">
+          <h1 className="sm:text-7xl font-semibold font-sans text-neutral-50 text-xl text-start">
             Kreuj Przyszłość Książek z Future!
           </h1>
         </div>
-        <div></div>
+        <div className=" flex w-1/2 items-center justify-center">
+          <ul className="flex items-center justify-center gap-4">
+            <li>
+              <Link href="/login" className="text-neutral-100">
+                Log in
+              </Link>
+            </li>
+            <li>
+              <Link href="/#" className="text-neutral-100">
+                Read more
+              </Link>
+            </li>
+          </ul>
+        </div>
       </section>
     </>
   );
