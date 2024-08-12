0d1afd243e4a0f41487dd1e52923de1688318891
pawelek <k.pawelek@vp.pl>
2024-08-12 19:19:40 +0200

    add sidebar genres

---
+++ app/components/home/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { bookGenres } from "@/lib/routes";
+import { Divide as Hamburger } from "hamburger-react";
+import Link from "next/link";
+import { useState } from "react";
+import { SecondHeading } from "../global/heading";
+
+export const Sidebar: React.FC = () => {
+  const [isOpen, setOpen] = useState(true);
+
+  return (
+    <div>
+      <div className={`fixed z-50 top-2 left-1/2 transform -translate-x-1/2`}>
+        <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
+      </div>
+
+      <div
+        className={`fixed top-0 right-0 h-full text-sky-950  bg-neutral-100/20 dark:bg-zinc-950/20 p-4 pt-24 z-40 transition-transform duration-300 ease-in-out backdrop-blur-lg ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+        style={{ width: "250px" }}
+      >
+        <SecondHeading>Genres</SecondHeading>
+        <ul className="space-y-2 mt-6">
+          {bookGenres.map(({ id, name }) => (
+            <li
+              key={id}
+              className="hover:bg-neutral-100/10 rounded transition-colors w-full dark:hover:bg-zinc-800/10"
+            >
+              <Link
+                href={`/home/${id}`}
+                className="gap-4 p-2 w-full h-full text-sky-950 flex items-center hover:text-neutral-400 transition-all dark:text-neutral-50"
+              >
+                <span className="text-sm">{name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,11 @@
 import { FirstHeading } from "@/app/components/global/heading";
+import { Sidebar } from "@/app/components/home/Sidebar";
 
 export default function HomeAuthPage() {
   return (
     <div>
       <FirstHeading>The most popular books</FirstHeading>
+      <Sidebar />
     </div>
   );
 }
