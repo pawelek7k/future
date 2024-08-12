17bea11cd7ad8845d8310aa113d8854dbad5149d
pawelek <k.pawelek@vp.pl>
2024-08-12 15:53:40 +0200

    add fixes

---
+++ pages/home/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeAuthPage() {
+  return (
+    <div>
+      <h1>ok</h1>
+    </div>
+  );
+}
--- pages/home/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomeAuthPage() {
-  return <div></div>;
-}
