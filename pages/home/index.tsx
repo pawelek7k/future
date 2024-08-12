d4b4555c24a170f0b81b5b4dafdd742265907552
pawelek <k.pawelek@vp.pl>
2024-08-12 19:32:40 +0200

    Update index.tsx

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,11 +1,18 @@
 import { FirstHeading } from "@/app/components/global/heading";
 import { Sidebar } from "@/app/components/home/Sidebar";
+import Head from "next/head";
 
 export default function HomeAuthPage() {
   return (
-    <div>
-      <FirstHeading>The most popular books</FirstHeading>
-      <Sidebar />
-    </div>
+    <>
+      <Head>
+        <title>Future - Home</title>
+        <meta name="description" content="Future" />
+      </Head>
+      <div>
+        <FirstHeading>The most popular books</FirstHeading>
+        <Sidebar />
+      </div>
+    </>
   );
 }
