b1b4a60511d1941bf6211a76e605e4a7ffa46a85
pawelek <k.pawelek@vp.pl>
2024-08-13 11:56:34 +0200

    add fixes

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,8 +1,25 @@
 import { FirstHeading } from "@/app/components/global/heading";
 import { Sidebar } from "@/app/components/home/Sidebar";
+import { connectToDatabase } from "@/lib/db";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
-export default function HomeAuthPage() {
+interface Book {
+  _id: string;
+  title: string;
+  cover: string;
+  description: string;
+  forAdult: boolean;
+  genre: string;
+  tags: string[];
+}
+
+interface BooksPageProps {
+  books: Book[];
+}
+
+const HomeAuthPage = ({ books }: BooksPageProps) => {
   return (
     <>
       <Head>
@@ -12,7 +29,51 @@ export default function HomeAuthPage() {
       <div>
         <FirstHeading>The most popular books</FirstHeading>
         <Sidebar />
+        <ul>
+          {books.map((book) => (
+            <li key={book._id}>
+              <h2>{book.title}</h2>
+              <Image
+                src={book.cover}
+                alt={book.title}
+                style={{ maxWidth: "150px", height: "230px" }}
+                width={150}
+                height={300}
+              />
+              <p>{book.description}</p>
+              <p>Genre: {book.genre}</p>
+              <p>For Adult: {book.forAdult ? "Yes" : "No"}</p>
+              <p>Tags: {book.tags.join(", ")}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const client = await connectToDatabase();
+    const db = client.db();
+    const booksCollection = db.collection("books");
+    const books = await booksCollection.find({}).toArray();
+
+    client.close();
+
+    return {
+      props: {
+        books: JSON.parse(JSON.stringify(books)),
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: {
+        books: [],
+      },
+    };
+  }
+};
+
+export default HomeAuthPage;
