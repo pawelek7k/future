4c34dd3d01d02cac15cf27186cf148c805d90ea8
pawelek <k.pawelek@vp.pl>
2024-08-14 10:08:36 +0200

    add secondary btn

--- app/components/global/buttons/index.tsx
+++ app/components/global/buttons/index.tsx
@@ -76,12 +76,25 @@ export const AbsoluteButton: React.FC<ButtonProps> = ({
   );
 };
 
-export const HeroButton = () => {
+export const HeroButton: React.FC = () => {
   return (
     <button className="border border-neutral-100 backdrop-blur-md py-2 px-6 transition easy-in-out font-sans tracking-wide rounded-full shadow-md text-neutral-100 shadow-neutral-100/30 hover:bg-neutral-100 hover:text-black text-large">
       <Link href="/login" className="">
         Log in
       </Link>
+    </button>
+  );
+};
+
+export const SecondaryButton: React.FC<ButtonProps> = ({ children }) => {
+  return (
+    <button
+      className="mt-4 p-2 absolute -top-10 right-0  bg-sky-900 text-white py-2 px-4 rounded-lg
+hover:bg-sky-950
+dark:bg-rose-900 dark:hover:bg-rose-800
+shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out"
+    >
+      {children}
     </button>
   );
 };
