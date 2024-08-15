ae2b122a286766d476213c469c1b35a09b1379a6
pawelek <k.pawelek@vp.pl>
2024-08-15 14:09:45 +0200

    Update MainNavigation.tsx

--- app/components/layout/MainNavigation.tsx
+++ app/components/layout/MainNavigation.tsx
@@ -2,6 +2,7 @@ import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import { CiCirclePlus, CiSettings } from "react-icons/ci";
+import { IoLibrary } from "react-icons/io5";
 import { Logo } from "../../components/global/Logo";
 
 export const MainNavigation: React.FC = () => {
@@ -23,6 +24,11 @@ export const MainNavigation: React.FC = () => {
               <li>
                 <Link href="/home/create">
                   <CiCirclePlus className="w-6 h-6" />
+                </Link>
+              </li>
+              <li>
+                <Link href="/home/library">
+                  <IoLibrary className="w-6 h-6" />
                 </Link>
               </li>
               <li>
