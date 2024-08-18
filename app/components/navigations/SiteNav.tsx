aa5094d3b8e14595e2807bcbdfe74f8287edbe3c
pawelek <k.pawelek@vp.pl>
2024-08-18 22:17:03 +0200

    Update SiteNav.tsx

--- app/components/navigations/SiteNav.tsx
+++ app/components/navigations/SiteNav.tsx
@@ -13,23 +13,23 @@ export const SiteNavigation: React.FC = () => {
 
   return (
     <header className="flex justify-evenly p-4 w-full fixed z-40 bg-secondaryBg backdrop-blur-md rounded-b-lg border-b top-0 items-center dark:border-b-zinc-800 shadow-lg">
-      <nav className="flex items-center">
+      <nav className="flex items-center justify-evenly w-full">
         <Logo />
         <ul className="flex gap-8 tracking-widest justify-evenly">
           <li>
-            <Link href="/home/create" className="flex items-center">
+            <Link href="/home/create" className="flex items-center gap-2">
               <CiCirclePlus className="w-6 h-6" />
               <span>Create</span>
             </Link>
           </li>
           <li>
-            <Link href="/home/library" className="flex items-center">
+            <Link href="/home/library" className="flex items-center gap-2">
               <IoLibrary className="w-6 h-6" />
               <span>Library</span>
             </Link>
           </li>
           <li>
-            <Link href="/home/settings" className="flex items-center">
+            <Link href="/home/settings" className="flex items-center gap-2">
               <CiSettings className="w-6 h-6" />
               <span>Settings</span>
             </Link>
