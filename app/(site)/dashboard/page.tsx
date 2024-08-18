9b81d8eeb31ff3b1e1d359a2602ad2189eeb5f89
pawelek <k.pawelek@vp.pl>
2024-08-18 21:48:39 +0200

    Update page.tsx

--- app/(site)/dashboard/page.tsx
+++ app/(site)/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import { connectToDatabase } from "@/lib/db";
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import ClientSideComponent from "@/app/components/login/home/ClientSideComponent";
+import { connectToDatabase } from "@/lib/db";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
 
 interface Book {
   _id: string;
@@ -30,10 +30,31 @@ export default async function HomeAuthPage() {
   const db = client.db();
   const booksCollection = db.collection("books");
 
-  const books = await booksCollection.find({}).toArray();
-  const booksWithId = books.map((book) => ({
-    ...book,
+  const books = await booksCollection
+    .find(
+      {},
+      {
+        projection: {
+          _id: 1,
+          title: 1,
+          cover: 1,
+          description: 1,
+          forAdult: 1,
+          genre: 1,
+          tags: 1,
+        },
+      }
+    )
+    .toArray();
+
+  const booksWithId: Book[] = books.map((book) => ({
     _id: book._id.toString(),
+    title: book.title,
+    cover: book.cover,
+    description: book.description,
+    forAdult: book.forAdult,
+    genre: book.genre,
+    tags: book.tags,
   }));
 
   return <ClientSideComponent books={booksWithId} session={session} />;
