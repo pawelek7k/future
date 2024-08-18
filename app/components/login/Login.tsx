28448c582b3785eb4638eff5757d31787bffde47
pawelek <k.pawelek@vp.pl>
2024-08-18 16:41:16 +0200

    Update Login.tsx

--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React from "react";
+
 interface FormData {
   email: string;
   username?: string;
@@ -18,45 +20,50 @@ export const LoginForm: React.FC<LoginFormProps> = ({
   submitHandler,
 }) => (
   <div className="bg-neutral-50/20 dark:bg-zinc-900/20 text-gray-900 dark:text-neutral-50 p-8 rounded-lg shadow-lg w-full max-w-md mx-auto mt-10">
-    <h1>Logowanie</h1>
+    <h1 className="text-2xl font-semibold mb-6">Logowanie</h1>
     <form onSubmit={submitHandler}>
       <div className="mb-4">
         <label
-          htmlFor="email2"
+          htmlFor="email"
           className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
         >
           E-mail
         </label>
         <input
           type="email"
-          id="email2"
+          id="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
           placeholder="Wpisz swój e-mail"
-          className="w-full px-3 py-2 border border-gray-300  rounded-lg bg-white text-gray-9 placeholder-gray-500  focus:outline-none focus:border-sky-950 "
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-sky-500"
         />
       </div>
       <div className="mb-4">
         <label
-          htmlFor="password2"
+          htmlFor="password"
           className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
         >
           Hasło
         </label>
         <input
           type="password"
-          id="password2"
+          id="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
           placeholder="Wpisz swoje hasło"
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white  text-gray-900  placeholder-gray-500  focus:outline-none focus:border-sky-950 "
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-sky-500"
         />
       </div>
       <ul className="flex flex-col gap-4">
         <li>
-          <button>Zaloguj się</button>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Zaloguj się
+          </button>
         </li>
         <li>{/* <GoogleButton /> */}</li>
       </ul>
