8642ad77c2e0d910c39808b75da7f1540a67f1eb
pawelek <k.pawelek@vp.pl>
2024-08-18 23:52:07 +0200

    add update book

---
+++ app/api/books/update/[id]/route.ts
@@ -0,0 +1,53 @@
+
+import { connectToDatabase } from "@/lib/db";
+import Joi from "joi";
+import { ObjectId } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+const contentSchema = Joi.object({
+    content: Joi.string().required()
+});
+
+export async function PUT(request: NextRequest) {
+    const url = new URL(request.url);
+    const id = url.pathname.split('/').pop();
+
+    if (!id || !ObjectId.isValid(id)) {
+        return NextResponse.json({ message: 'Invalid or missing book ID' }, { status: 400 });
+    }
+
+    const { content } = await request.json();
+    const { error, value } = contentSchema.validate({ content });
+
+    if (error) {
+        console.log("Validation error:", error.details);
+        return NextResponse.json({ message: error.details[0].message }, { status: 400 });
+    }
+
+    try {
+        const client = await connectToDatabase();
+        const db = client.db();
+        const booksCollection = db.collection('books');
+
+        console.log(`Updating book with ID: ${id}`);
+        console.log(`New content: ${content}`);
+
+        const result = await booksCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { content } }
+        );
+
+        console.log('Update result:', result);
+
+        client.close();
+
+        if (result.modifiedCount === 0) {
+            return NextResponse.json({ message: 'Book not found or no changes made' }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: 'Content updated!' }, { status: 200 });
+    } catch (err) {
+        console.error('Error updating content:', err);
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
+}
