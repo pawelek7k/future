af5786d144ad082e87998a3c749599c819c82086
pawelek <k.pawelek@vp.pl>
2024-08-19 09:39:48 +0200

    add quill editor from react

---
+++ app/components/editor/index.tsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { SetStateAction, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+interface RichTextEditorProps {
+  bookId: string;
+}
+
+interface RichTextEditorStaticProps {
+  modules: any;
+  formats: any;
+}
+
+export const RichTextEditor: React.FC<RichTextEditorProps> &
+  RichTextEditorStaticProps = ({ bookId }) => {
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (value: SetStateAction<string>) => {
+    setText(value);
+  };
+
+  const handleSave = async () => {
+    if (!bookId) {
+      console.error("No book ID provided");
+      alert("Failed to save content: Book ID is missing");
+      return;
+    }
+
+    try {
+      await axios.put(`/api/books/update/${bookId}`, { content: text });
+      alert("Content saved successfully!");
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "Error saving content:",
+          error.response?.data || error.message
+        );
+        alert("Failed to save content");
+      } else {
+        console.error("Unexpected error:", error);
+        alert("An unexpected error occurred");
+      }
+    }
+  };
+
+  return (
+    <div className="w-full min-h-screen p-10 relative">
+      <ReactQuill
+        value={text}
+        onChange={handleChange}
+        modules={RichTextEditor.modules}
+        formats={RichTextEditor.formats}
+        placeholder="Once upon a time lived a king who had three daughters..."
+      />
+      <button onClick={handleSave}>Save</button>
+    </div>
+  );
+};
+
+RichTextEditor.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ align: [] }],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+RichTextEditor.formats = [
+  "header",
+  "font",
+  "list",
+  "bullet",
+  "bold",
+  "italic",
+  "underline",
+  "color",
+  "background",
+  "align",
+  "link",
+  "image",
+];
