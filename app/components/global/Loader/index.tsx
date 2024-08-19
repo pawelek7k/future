add6d324585002660cf6b806395f66aff9a5beb1
pawelek <k.pawelek@vp.pl>
2024-08-19 13:46:27 +0200

    add fixes

--- app/components/global/Loader/index.tsx
+++ app/components/global/Loader/index.tsx
@@ -2,7 +2,7 @@ import "./Loader.css";
 
 export const Loader: React.FC = () => {
   return (
-    <div>
+    <div className="fixed w-full -top-3 z-50 left-0">
       <span className="loader"></span>
     </div>
   );
