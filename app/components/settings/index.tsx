b012f8c0d5f8e6819faa9c59d65d670be1e788ea
pawelek <k.pawelek@vp.pl>
2024-08-20 17:48:16 +0200

    Update index.tsx

--- app/components/settings/index.tsx
+++ app/components/settings/index.tsx
@@ -1,11 +1,24 @@
-"use client";
-
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { SecondHeading, ThirdHeading } from "../global/Heading";
 import { DarkModeSwitch } from "./Darkmode";
 
-export const GlobalSettings: React.FC = () => {
+export const GlobalSettings: React.FC = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <>
       <DarkModeSwitch />
+      <div>
+        <div className="w-32 h-32 border border-sky-950 rounded-full"></div>
+        <ThirdHeading>display name</ThirdHeading>
+        <SecondHeading>{session.user?.username}</SecondHeading>
+      </div>
     </>
   );
 };
