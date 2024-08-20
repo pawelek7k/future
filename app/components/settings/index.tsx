f5d68d344ea9fca093495c605fd474ab0c291f0a
pawelek <k.pawelek@vp.pl>
2024-08-20 22:18:57 +0200

    Update index.tsx

--- app/components/settings/index.tsx
+++ app/components/settings/index.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { SecondHeading, ThirdHeading } from "../global/Heading";
+import { SecondHeading } from "../global/Heading";
 import { DarkModeSwitch } from "./Darkmode";
 
 export const GlobalSettings: React.FC = async () => {
@@ -14,13 +14,34 @@ export const GlobalSettings: React.FC = async () => {
   return (
     <>
       <DarkModeSwitch />
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <div className="w-32 h-32 border border-sky-950 rounded-full"></div>
-        <ThirdHeading>display name</ThirdHeading>
-        <SecondHeading>{session.user?.username}</SecondHeading>
-        <p>Followers:</p>
-        <p>Following:</p>
-        <p>books:</p>
+        <div className="text-center flex flex-col gap-2">
+          <SecondHeading>display name</SecondHeading>
+          <p>{session.user?.username}</p>
+        </div>
+        <div>
+          <ul className="flex w-96 justify-between mt-4">
+            <li>
+              <button className="flex flex-col items-center">
+                <span>Followers:</span>
+                <span>0</span>
+              </button>
+            </li>
+            <li>
+              <button className="flex flex-col items-center">
+                <span>Work:</span>
+                <span>0</span>
+              </button>
+            </li>
+            <li>
+              <button className="flex flex-col items-center">
+                <span>Following:</span>
+                <span>0</span>
+              </button>
+            </li>
+          </ul>
+        </div>
         <p>User Description:</p>
       </div>
     </>
