08111718a32bed83035057ff67133d39acec3392
pawelek <k.pawelek@vp.pl>
2024-08-20 16:47:15 +0200

    solved problem

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <NextAuthProvider>
         <body className="bg-primary-bg dark:bg-dark-primary-bg min-h-screen">
           <ThemeProvider
