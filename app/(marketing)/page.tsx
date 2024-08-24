b00500307fbdcf5d29916090bc68e288b31c423a
pawelek <k.pawelek@vp.pl>
2024-08-24 16:49:26 +0200

    add hero bg to first page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,6 @@
 const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-hero-bg bg-center bg-no-repeat bg-cover"></main>
   );
 };
 export default Home;
