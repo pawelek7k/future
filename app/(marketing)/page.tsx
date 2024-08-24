bb433dd6045e56fe2d6e0102adb8afc427aa364e
pawelek <k.pawelek@vp.pl>
2024-08-24 17:06:33 +0200

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,3 @@
-import { FirstHeading } from "../components/global/Heading";
 import { MarketingNavigation } from "../components/navigations/MarketingNav";
 
 const Home: React.FC = () => {
@@ -6,7 +5,13 @@ const Home: React.FC = () => {
     <main className="">
       <section className="h-screen bg-hero-bg bg-center bg-no-repeat bg-cover m-1 rounded-lg">
         <MarketingNavigation />
-        <FirstHeading>Your way</FirstHeading>
+        <p className="text-neutral-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, sint.
+        </p>
+        <h1 className="text-neutral-100 font-sans text-7xl">
+          Create the Future of Books!
+        </h1>
       </section>
     </main>
   );
