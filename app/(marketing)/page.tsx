69d9ee7c1e7143ec6e5aa9fb034c1bdf3a72504d
pawelek <k.pawelek@vp.pl>
2024-08-24 17:34:41 +0200

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,12 +15,12 @@ const Home: React.FC = () => {
           </h1>
           <ul className="flex gap-10">
             <li>
-              <button className="text-neutral-100 bg-sky-950 px-8 py-2 rounded-lg">
+              <button className="text-neutral-100 bg-sky-950 px-8 py-2 rounded-lg text-nowrap">
                 Log in!
               </button>
             </li>
             <li>
-              <button className="text-neutral-100 border border-neutral-100 px-8 py-2 rounded-lg">
+              <button className="text-neutral-100 border border-neutral-100 px-8 py-2 rounded-lg text-nowrap">
                 Read more
               </button>
             </li>
@@ -30,11 +30,31 @@ const Home: React.FC = () => {
             Reprehenderit, sint.
           </p>
         </div>
-        <div className="flex items-center justify-center p-20">
-          <div className="rounded-lg border border-neutral-300 p-10 shadow-lg shadow-white backdrop-blur-lg">
+        <div className="flex items-center justify-center p-40">
+          <div className="rounded-lg border border-neutral-300 p-10 shadow-lg shadow-white backdrop-blur-lg h-full">
+            <p className="text-neutral-300">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.{" "}
+            </p>
             <h2 className="text-neutral-100  text-3xl ">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.{" "}
             </h2>
+            <p className="text-neutral-300 w-1/2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              distinctio porro illum saepe perferendis reiciendis sunt unde
+              expedita, commodi necessitatibus, magnam iusto corporis beatae
+              tempore molestias exercitationem odio quidem sapiente.
+            </p>
+            <ul className="flex justify-between text-neutral-100">
+              <li>
+                <button>Lorem</button>
+              </li>
+              <li>
+                <button>Lorem</button>
+              </li>
+              <li>
+                <button>Lorem</button>
+              </li>
+            </ul>
           </div>
         </div>
       </section>
