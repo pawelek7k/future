a72d2b34d921c4c7b31247639661d5823c81b608
pawelek <k.pawelek@vp.pl>
2024-08-25 13:43:50 +0200

    add singup form styles

--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { FaFacebookSquare, FaPinterest } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
--- app/components/login/Signup.tsx
+++ app/components/login/Signup.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaFacebookSquare, FaPinterest } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import { PrimaryButton } from "../global/Buttons";
 
 interface SignupFormProps {
@@ -15,37 +17,84 @@ export const SignupForm: React.FC<SignupFormProps> = ({
   formData,
   handleChange,
   submitHandler,
-}) => {
-  return (
-    <form
-      onSubmit={submitHandler}
-      className="bg-neutral-50 dark:bg-zinc-900 text-gray-900 flex flex-col dark:text-neutral-50 p-8 rounded-lg shadow-lg w-full max-w-md mx-auto mt-10 gap-6"
-    >
-      <input
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="Enter your e-mail"
-        className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
-      />
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-        placeholder="Enter your username"
-        className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        placeholder="Enter your password"
-        className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
-      />
+}) => (
+  <div className="bg-zinc-950/90 backdrop-blur-md p-8 rounded-lg shadow-lg w-full max-w-md mx-auto mt-10 shadow-rose-950">
+    <h1 className="text-2xl font-semibold text-neutral-100 text-center">
+      Join us!
+    </h1>
+    <p className="text-neutral-100 bg-sky-950 p-1 rounded-full shadow-lg text-center mb-6">
+      Create your account now.
+    </p>
+    <form onSubmit={submitHandler} className="">
+      <div className="mb-4">
+        <label
+          htmlFor="email"
+          className="block text-neutral-100 text-sm font-medium mb-2"
+        >
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Enter your e-mail"
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="username"
+          className="block text-neutral-100 text-sm font-medium mb-2"
+        >
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Enter your username"
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="password"
+          className="block text-neutral-100 text-sm font-medium mb-2"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Enter your password"
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:border-rose-950"
+        />
+      </div>
       <PrimaryButton>Sign Up</PrimaryButton>
+      <ul className="flex gap-10 md:items-center md:mt-10 mb-10 md:mb-0 justify-center">
+        <li className="rounded-full p-3 border border-rose-950">
+          <a href="">
+            <FaFacebookSquare className="w-6 h-6 text-neutral-100 hover:text-neutral-300 transition ease-in-out" />
+          </a>
+        </li>
+        <li className="rounded-full p-3 border border-rose-950">
+          <a href="">
+            <FaXTwitter className="w-6 h-6 text-neutral-100 hover:text-neutral-300 transition ease-in-out" />
+          </a>
+        </li>
+        <li className="rounded-full p-3 border border-rose-950">
+          <a href="">
+            <FaPinterest className="w-6 h-6 text-neutral-100 hover:text-neutral-300 transition ease-in-out" />
+          </a>
+        </li>
+      </ul>
     </form>
-  );
-};
+  </div>
+);
