640d735c53041969e87d3286246f333d9a33b377
pawelek <k.pawelek@vp.pl>
2024-08-26 12:09:55 +0200

    Update SearchInput.tsx

--- app/components/global/SearchInput.tsx
+++ app/components/global/SearchInput.tsx
@@ -13,7 +13,7 @@ export const SearchInput: React.FC<Props> = ({
 }) => {
   return (
     <div className="relative">
-      <CiSearch className="absolute top-1/2 left-3 transform -translate-y-1/2" />
+      <CiSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 " />
       <input
         value={value}
         onKeyDown={onKeyDown}
@@ -21,7 +21,7 @@ export const SearchInput: React.FC<Props> = ({
         type="text"
         name="text"
         placeholder="Search..."
-        className="pl-10 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-sky-950 w-full"
+        className="pl-10 rounded-lg focus:outline-none w-full dark:focus:border-rose-950 px-3 py-2 border border-gray-300 bg-neutral-100 text-gray-900 placeholder-gray-500 focus:border-sky-950"
       />
     </div>
   );
