3d3c9916fcbad5f7697d723d5c94c1f7e4418393
pawelek <k.pawelek@vp.pl>
2024-08-26 21:26:08 +0200

    Update Features.tsx

--- app/components/landing/Features.tsx
+++ app/components/landing/Features.tsx
@@ -2,23 +2,43 @@ import { SecondHeading, ThirdHeading } from "../global/Heading";
 
 export const Features = () => {
   return (
-    <section>
-      <SecondHeading>How Future Works?</SecondHeading>
-      <ul>
+    <section className="py-20 px-12">
+      <div className="text-center">
+        <SecondHeading>How Future Works?</SecondHeading>
+      </div>
+      <ul className="flex">
         <li>
           <div>
             <ThirdHeading>Sing up</ThirdHeading>
+            <p className="text-neutral-300">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+              non, quod minus accusantium impedit ea hic! Atque, distinctio nam
+              modi et minima, vero corrupti non enim libero voluptatibus magni.
+              Voluptatibus.
+            </p>
           </div>
         </li>
         <li>
           <div>
             {" "}
             <ThirdHeading>Create</ThirdHeading>
+            <p className="text-neutral-300">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+              non, quod minus accusantium impedit ea hic! Atque, distinctio nam
+              modi et minima, vero corrupti non enim libero voluptatibus magni.
+              Voluptatibus.
+            </p>
           </div>
         </li>
         <li>
           <div>
             <ThirdHeading>Read!</ThirdHeading>
+            <p className="text-neutral-300">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+              non, quod minus accusantium impedit ea hic! Atque, distinctio nam
+              modi et minima, vero corrupti non enim libero voluptatibus magni.
+              Voluptatibus.
+            </p>
           </div>
         </li>
       </ul>
