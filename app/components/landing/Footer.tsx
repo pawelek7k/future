4dc4475299b794c295d12d25805a999c8c447d1e
pawelek <k.pawelek@vp.pl>
2024-09-02 14:30:54 +0200

    Update Footer.tsx

--- app/components/landing/Footer.tsx
+++ app/components/landing/Footer.tsx
@@ -1,3 +1,15 @@
+import { LightLogo } from "../global/Logo";
+
 export const Footer = () => {
-  return <footer className="h-screen"></footer>;
+  return (
+    <footer className="h-screen p-20 border-t border-sky-950">
+      <div className="flex justify-start items-center gap-2">
+        <LightLogo />
+        <span className="text-neutral-300 font-semibold">
+          {" "}
+          - Create your way with future
+        </span>
+      </div>
+    </footer>
+  );
 };
