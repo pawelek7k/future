28e0023df86bcef3b9fcfaeb4de29856b66bdda7
pawelek <k.pawelek@vp.pl>
2024-09-04 17:31:34 +0200

    Update Cards.tsx

--- app/components/landing/Cards.tsx
+++ app/components/landing/Cards.tsx
@@ -1,6 +1,6 @@
 export const Cards: React.FC = () => {
   return (
-    <div className="flex justify-end border-l border-sky-950">
+    <div className=" justify-end border-l border-sky-950 hidden md:flex">
       <ul className="flex flex-col gap-10 pl-10">
         <li>
           <div className="text-neutral-100 card shadow-lg shadow-rose-700 flex flex-col">
