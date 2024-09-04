48b9750f8ec302f69338944be231a54c7759f33d
pawelek <k.pawelek@vp.pl>
2024-09-04 15:59:22 +0200

    Update Footer.tsx

--- app/components/landing/Footer.tsx
+++ app/components/landing/Footer.tsx
@@ -29,31 +29,60 @@ export const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="flex items-end justify-end p-10 border-t border-sky-950 mt-20 flex-col gap-6">
-        <LightLogo />
-        <p className="text-neutral-300">Contact with us!</p>
-        <ul className="flex flex-col gap-6">
-          <li>
-            <a href="https://www.facebook.com/" target="_blank">
-              <FaFacebookSquare className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
-            </a>
-          </li>
-          <li>
-            <a href="https://x.com/" target="_blank">
-              <FaXTwitter className="w-6 h-6 text-neutral-300 hover:text-sky-950 transition ease-in-out" />
-            </a>
-          </li>
-          <li>
-            <a href="https://pl.pinterest.com/" target="_blank">
-              <FaPinterest className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <HiShare className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
-            </a>
-          </li>
-        </ul>
+      <div className="flex justify-between border-t border-sky-950 mt-20 p-10">
+        <div className="text-neutral-300 ">
+          <h3 className="text-neutral-100 font-semibold text-xl mb-6">
+            Company
+          </h3>
+          <ul className="flex flex-col gap-6">
+            <li>About us</li>
+            <li>News</li>
+            <li>Contact</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div className="text-neutral-300 ">
+          <h3 className="text-neutral-100 font-semibold text-xl mb-6">
+            Company
+          </h3>
+          <ul className="flex flex-col gap-6">
+            <li>About us</li>
+            <li>News</li>
+            <li>Contact</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div className="flex items-end justify-end   flex-col gap-6">
+          <LightLogo />
+          <p className="text-neutral-300">Contact with us!</p>
+          <ul className="flex flex-col gap-6">
+            <li>
+              <a href="https://www.facebook.com/" target="_blank">
+                <FaFacebookSquare className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/" target="_blank">
+                <FaXTwitter className="w-6 h-6 text-neutral-300 hover:text-sky-950 transition ease-in-out" />
+              </a>
+            </li>
+            <li>
+              <a href="https://pl.pinterest.com/" target="_blank">
+                <FaPinterest className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <HiShare className="w-6 h-6 text-neutral-300 hover:text-sky-950  transition ease-in-out" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex items-end">
+        <p className="text-neutral-300 text-sm">
+          <span>&copy; 2024</span> - Future. All rights reserved.
+        </p>
       </div>
     </footer>
   );
