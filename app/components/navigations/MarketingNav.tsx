88e8b6cac3cce2de849a338bc7c7f381142eda24
pawelek <k.pawelek@vp.pl>
2024-09-05 13:59:48 +0200

    Update MarketingNav.tsx

--- app/components/navigations/MarketingNav.tsx
+++ app/components/navigations/MarketingNav.tsx
@@ -2,7 +2,7 @@ import { LightLogo } from "../global/Logo";
 
 export const MarketingNavigation: React.FC = () => {
   return (
-    <header className="flex justify-between p-4 fixed top-0 w-full backdrop-blur-md left-0">
+    <header className="flex justify-between p-4 fixed top-0 w-full backdrop-blur-md left-0 right-0 z-40 ">
       <LightLogo />
     </header>
   );
