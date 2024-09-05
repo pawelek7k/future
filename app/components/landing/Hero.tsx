f18226c9f25b34de150dcdaa3e967907116f5da5
pawelek <k.pawelek@vp.pl>
2024-09-05 14:05:01 +0200

    Update Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -9,10 +9,9 @@ export const Hero: React.FC = () => {
     <section className="p-28 h-screen bg-black bg-hero-bg">
       <MarketingNavigation />
       <div className="flex md:justify-between justify-center">
-        <div className="flex flex-col gap-8">
-          <h1 className="text-neutral-100 text-3xl sm:text-4xl lg:text-6xl w-1/2 text-center md:text-start">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-            nesciunt atque sint et! Aspernatur est, id, quos atque, molestiae
+        <div className="flex flex-col gap-8 justify-center">
+          <h1 className="text-neutral-100 text-3xl sm:text-4xl lg:text-6xl text-center md:text-start">
+            Create your way with Future!
           </h1>
           <ul className="text-neutral-100 gap-4 flex">
             <li>
