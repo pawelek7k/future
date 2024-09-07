0f4081a536dbc7dacf7bd3cb65fd1fb4191744cc
pawelek <k.pawelek@vp.pl>
2024-09-07 19:11:09 +0200

    Create statistics.ts

---
+++ lib/arrays/statistics.ts
@@ -0,0 +1,23 @@
+interface statisticsItem {
+    id: number,
+    numbers: string,
+    description: string
+}
+
+export const statistics: statisticsItem[] = [
+    {
+        id: 1,
+        numbers: '5k',
+        description: 'writers & readers'
+    },
+    {
+        id: 1,
+        numbers: '100',
+        description: 'books'
+    },
+    {
+        id: 1,
+        numbers: '100k',
+        description: 'views'
+    }
+]
