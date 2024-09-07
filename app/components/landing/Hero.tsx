5a92e45ca2179d391a1fe857570c0aa0436dff32
pawelek <k.pawelek@vp.pl>
2024-09-07 20:02:30 +0200

    Update Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -12,9 +12,15 @@ export const Hero: React.FC = () => {
       <MarketingNavigation />
       <div className="flex md:justify-between justify-center">
         <div className="flex flex-col gap-8 justify-center">
-          <h1 className="text-neutral-100 text-3xl sm:text-4xl lg:text-8xl w-1/2 text-center md:text-start">
+          <h1 className="text-neutral-100 text-3xl sm:text-4xl lg:text-8xl w-1/2 text-center md:text-start leading-loose">
             Create your way with Future!
           </h1>
+          <p className="text-neutral-300 w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            repellendus officiis! Vero eum quisquam beatae, odit asperiores,
+            officia ut doloremque voluptatum rem ab quo soluta neque iste.
+            Minima, sed quisquam?
+          </p>
           <div className="text-neutral-100 bg-sky-950 rounded-full flex justify-between py-1 items-center px-6 border border-sky-800 w-96">
             <p className="flex items-center gap-4">
               Read More <FaArrowDown />
