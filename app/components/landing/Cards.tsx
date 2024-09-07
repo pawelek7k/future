638375b2829531f4431a6ef454d506e973ae1cfd
pawelek <k.pawelek@vp.pl>
2024-09-07 19:13:40 +0200

    Update Cards.tsx

--- app/components/landing/Cards.tsx
+++ app/components/landing/Cards.tsx
@@ -1,49 +1,22 @@
+import { statistics } from "@/lib/arrays/statistics";
+
 export const Cards: React.FC = () => {
   return (
     <div className=" justify-end border-l border-sky-950 hidden md:flex">
       <ul className="flex flex-col gap-10 pl-10">
-        <li>
-          <div className="text-neutral-100 card shadow-lg shadow-rose-700 flex flex-col">
-            <div className="z-30 flex items-start text-2xl font-sans">
-              <span className="text-2xl font-semibold">+</span>
-              <span className="text-5xl">5k</span>
+        {statistics.map((statistic) => (
+          <li key={statistic.id}>
+            <div className="text-neutral-100 card shadow-lg shadow-rose-700 flex flex-col">
+              <div className="z-30 flex items-start text-2xl font-sans">
+                <span className="text-2xl font-semibold">+</span>
+                <span className="text-5xl">{statistic.numbers}</span>
+              </div>
+              <div className="z-30 text-neutral-300">
+                {statistic.description}
+              </div>
             </div>
-            <div className="z-30 text-neutral-300">
-              <p>
-                writers<span className="font-semibold"> &</span>
-              </p>
-              <p>readers</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="text-neutral-100 card shadow-lg shadow-rose-700 flex flex-col">
-            <div className="z-30 flex items-start text-2xl font-sans">
-              <span className="text-2xl font-semibold">+</span>
-              <span className="text-5xl">5k</span>
-            </div>
-            <div className="z-30 text-neutral-300">
-              <p>
-                writers<span className="font-semibold"> &</span>
-              </p>
-              <p>readers</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="text-neutral-100 card shadow-lg shadow-rose-700 flex flex-col">
-            <div className="z-30 flex items-start text-2xl font-sans">
-              <span className="text-2xl font-semibold">+</span>
-              <span className="text-5xl">5k</span>
-            </div>
-            <div className="z-30 text-neutral-300">
-              <p>
-                writers<span className="font-semibold"> &</span>
-              </p>
-              <p>readers</p>
-            </div>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   );
