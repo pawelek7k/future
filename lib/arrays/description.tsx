d55a645ef439d1346d44ed48cd9abeec099498f4
pawelek <k.pawelek@vp.pl>
2024-09-07 21:22:32 +0200

    Create description.tsx

---
+++ lib/arrays/description.tsx
@@ -0,0 +1,13 @@
+import { IoMdLogIn } from "react-icons/io";
+
+interface descriptionItem {
+  icon: JSX.Element;
+  img: string;
+  description: string;
+}
+
+export const description: descriptionItem[] = [
+  { icon: <IoMdLogIn />, img: "", description: "" },
+  { icon: <IoMdLogIn />, img: "", description: "" },
+  { icon: <IoMdLogIn />, img: "", description: "" },
+];
