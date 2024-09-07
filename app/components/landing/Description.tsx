e650c13528450f4cfdb00164946b358482323b44
pawelek <k.pawelek@vp.pl>
2024-09-07 22:21:43 +0200

    Update Description.tsx

--- app/components/landing/Description.tsx
+++ app/components/landing/Description.tsx
@@ -7,35 +7,37 @@ import { useState } from "react";
 export const Description = () => {
   const [selectedTab, setSelectedTab] = useState(descriptions[0]);
   return (
-    <div className="border border-rose-950 h-[20rem] w-[40rem] rounded-lg">
-      <nav>
-        <ul className="text-neutral-100">
-          {descriptions.map((description) => (
-            <li
-              key={description.label}
-              onClick={() => setSelectedTab(description)}
+    <section className="flex items-center justify-center">
+      <div className="border border-rose-950 h-[20rem] w-[40rem] rounded-lg">
+        <nav>
+          <ul className="text-neutral-100">
+            {descriptions.map((description) => (
+              <li
+                key={description.label}
+                onClick={() => setSelectedTab(description)}
+              >
+                {`${description.icon} ${description.label}`}
+                {description === selectedTab ? (
+                  <motion.div className="underline" layoutId="underline" />
+                ) : null}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex justify-center items-center flex-1">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={selectedTab ? selectedTab.label : "empty"}
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{ duration: 0.2 }}
             >
-              {`${description.icon} ${description.label}`}
-              {description === selectedTab ? (
-                <motion.div className="underline" layoutId="underline" />
-              ) : null}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="flex justify-center items-center flex-1">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={selectedTab ? selectedTab.label : "empty"}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -10, opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            {selectedTab ? selectedTab.icon : "😋"}
-          </motion.div>
-        </AnimatePresence>
+              {selectedTab ? selectedTab.icon : "😋"}
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
