6922100075e3927faa642011e264d60ed305ec35
pawelek <k.pawelek@vp.pl>
2024-09-07 15:49:19 +0200

    Create quotes.ts

---
+++ lib/arrays/quotes.ts
@@ -0,0 +1,12 @@
+export const quotes = [
+    "Writers create universes from letters and words.",
+    "Every book is a journey crafted by the author and rediscovered by the reader.",
+    "Writing is the art of turning thoughts into something tangible.",
+    "Books are mirrors of the soul; we see in them what we carry within ourselves.",
+    "Create stories that linger in memory, even when the words fade.",
+    "The most beautiful stories are born where imagination knows no bounds.",
+    "When you write, you create a world that exists only because of you.",
+    "Writers don’t create stories; they uncover them.",
+    "Words have the power to transport us to places that exist only in our imagination.",
+    "The hardest part of writing is finding the silence where imagination speaks."
+];
