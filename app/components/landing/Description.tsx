5bc1ebf514030b548d4d958ee424c7b670a02154
pawelek <k.pawelek@vp.pl>
2024-09-07 22:25:01 +0200

    Update Description.tsx

--- app/components/landing/Description.tsx
+++ app/components/landing/Description.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export const Description = () => {
   const [selectedTab, setSelectedTab] = useState(descriptions[0]);
   return (
-    <section className="flex items-center justify-center">
+    <section className="flex items-center justify-center p-12">
       <div className="border border-rose-950 h-[20rem] w-[40rem] rounded-lg">
         <nav>
           <ul className="text-neutral-100 flex justify-between py-2 px-6 ">
