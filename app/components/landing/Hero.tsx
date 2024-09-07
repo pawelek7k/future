c2c8a29888591cfca890b8f0e403647fb6836db6
pawelek <k.pawelek@vp.pl>
2024-09-07 19:30:12 +0200

    Update Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
+import { FaArrowDown } from "react-icons/fa";
 import "../../styles/card.css";
-import { PrimaryButton, SecondaryButton } from "../global/Buttons";
 import { MarketingNavigation } from "../navigations/MarketingNav";
 import { Cards } from "./Cards";
 import { Quotes } from "./Quotes";
@@ -15,16 +15,18 @@ export const Hero: React.FC = () => {
           <h1 className="text-neutral-100 text-3xl sm:text-4xl lg:text-6xl text-center md:text-start">
             Create your way with Future!
           </h1>
-          <ul className="text-neutral-100 gap-4 flex">
-            <li>
-              <Link href="/login">
-                <PrimaryButton>Join us!</PrimaryButton>
-              </Link>
-            </li>
-            <li>
-              <SecondaryButton>Read more</SecondaryButton>
-            </li>
-          </ul>
+          <div className="text-neutral-100 bg-sky-950 rounded-full flex justify-between py-1 items-center px-6 border border-sky-800">
+            <p className="flex items-center gap-4">
+              Read More <FaArrowDown />
+            </p>
+            <span className="font-semibold">&</span>
+            <Link
+              href="/login"
+              className="py-2 px-8 border border-sky-800 rounded-full hover:bg-sky-800 transition ease-in-out"
+            >
+              Join us!
+            </Link>
+          </div>
         </div>
         <Cards />
       </div>
