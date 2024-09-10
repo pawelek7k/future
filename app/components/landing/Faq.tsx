47058c95a84f2de153dabfba437dc7e741d69231
pawelek <k.pawelek@vp.pl>
2024-09-10 18:42:20 +0200

    Update Faq.tsx

--- app/components/landing/Faq.tsx
+++ app/components/landing/Faq.tsx
@@ -1,4 +1,5 @@
 "use client";
+import accordionData from "@/lib/arrays/json/faq.json";
 import { motion } from "framer-motion";
 import { useRef, useState } from "react";
 
@@ -12,7 +13,7 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content }) => {
   const contentRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="border-b border-rose-950">
+    <div className="border-b border-rose-950 shadow-md shadow-rose-950">
       <button
         className="w-full text-left py-4 px-5 flex justify-between items-center focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}
@@ -35,31 +36,13 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content }) => {
 };
 
 export const Accordion: React.FC = () => {
-  const accordionData = [
-    {
-      title: "Is the writing app free?",
-      content:
-        "Yes, the app offers a free plan that allows you to write and manage a few books. There are also paid plans that provide additional features such as advanced editing, collaboration, and more storage space.",
-    },
-    {
-      title: "How does the writing app work?",
-      content:
-        "The writing app allows users to create books, chapters, and notes in an organized manner. You can write your content, format text, add notes, and collaborate with other writers in real time.",
-    },
-    {
-      title: "Can I export my books?",
-      content:
-        "Yes, the app allows you to export your books in various formats such as PDF or Word. This makes it easy to publish your content on different platforms or print your books.",
-    },
-  ];
-
   return (
-    <section className="max-w-2xl mx-auto mt-10 py-20 px-12 flex flex-col items-center gap-16">
+    <section className="max-w-2xl mx-auto mt-10 pb-20 pt-0 px-12 flex flex-col items-center gap-16">
       <h2 className="text-neutral-100 font-semibold text-4xl text-center">
         FAQ
       </h2>
       <div>
-        {accordionData.map((item, index) => (
+        {accordionData.accordionData.map((item, index) => (
           <AccordionItem
             key={index}
             title={item.title}
