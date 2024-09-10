a69948067b715b7a5b26160c17502ecd4a9a0804
pawelek <k.pawelek@vp.pl>
2024-09-10 17:33:27 +0200

    Update Reviews.tsx

--- app/components/landing/Reviews.tsx
+++ app/components/landing/Reviews.tsx
@@ -9,31 +9,34 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 export const Reviews: React.FC = () => {
   return (
-    <section>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide>
-          <div className="bg-neutral-100 w-52 h-32"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="bg-neutral-100 w-52 h-32"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="bg-neutral-100 w-52 h-32"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="bg-neutral-100 w-52 h-32"></div>
-        </SwiperSlide>
-      </Swiper>
+    <section className="py-20 px-12 flex flex-col items-center gap-16 justify-center">
+      <h2 className="text-neutral-100 font-semibold text-4xl">Reviews</h2>
+      <div className="w-[750px] flex justify-center items-center">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            <div className="bg-neutral-100 w-52 h-32"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-neutral-100 w-52 h-32"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-neutral-100 w-52 h-32"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-neutral-100 w-52 h-32"></div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </section>
   );
 };
