1cb7a6c0a0223f692002a91202b6974f448e0547
pawelek <k.pawelek@vp.pl>
2024-09-11 16:54:24 +0200

    Update Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -15,17 +15,25 @@ export const Hero: React.FC = () => {
           <h1 className="text-neutral-100 text-3xl sm:text-6xl lg:text-8xl w-1/2 text-center md:text-start leading-loose">
             Create your way with Future!
           </h1>
-          <div className="text-neutral-100 bg-sky-950 rounded-full flex justify-between py-1 items-center px-6 border border-sky-800 w-96">
-            <p className="flex items-center gap-4">
-              Read More <FaArrowDown />
-            </p>
-            <span className="font-semibold">&</span>
-            <Link
-              href="/login"
-              className="py-2 px-8 border border-sky-800 rounded-full hover:bg-sky-800 transition ease-in-out"
-            >
-              Join us!
-            </Link>
+          <div className="text-neutral-100 rounded-full w-96 shadow-lg backdrop-blur-lg shadow-sky-950">
+            <ul className="flex justify-between px-8 py-4">
+              <li>
+                <button className="flex items-center gap-4">
+                  Read More <FaArrowDown />
+                </button>
+              </li>
+              <li>
+                <span className="font-semibold">&</span>
+              </li>
+              <li>
+                <Link
+                  href="/login"
+                  className="py-2 px-8 border border-sky-800 rounded-full hover:bg-sky-800 transition ease-in-out"
+                >
+                  Join us!
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
         <Cards />
