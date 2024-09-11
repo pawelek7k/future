5e202fd77fb4f7d118cb0f783a38976a0e310127
pawelek <k.pawelek@vp.pl>
2024-09-11 17:02:12 +0200

    add fixes

--- app/components/global/Logo.tsx
+++ app/components/global/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo: React.FC = () => (
   <Link href="/home">
     <div className="flex items-center gap-4 justify-center">
       <IoMdMoon className="w-8 h-8" />
-      <h1 className="text-lg font-sans tracking-widest hidden lg:block">
+      <h1 className="text-lg font-sans tracking-widest hidden lg:block font-semibold">
         Future
       </h1>
     </div>
@@ -16,7 +16,7 @@ export const LightLogo: React.FC = () => (
   <Link href="/">
     <div className="flex items-center gap-4 justify-center">
       <IoMdMoon className="w-8 h-8 text-neutral-100" />
-      <h1 className="text-3xl font-sans text-neutral-100 tracking-widest hidden fancy lg:block">
+      <h1 className="text-3xl font-sans text-neutral-100 tracking-widest hidden fancy lg:block font-semibold">
         Future
       </h1>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
       },
     },
     fontFamily: {
-      'sans': ["Spicy Rice", 'serif']
+      'sans': ["SUSE", 'sans-serif']
     }
   },
   plugins: [],
