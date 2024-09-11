0203dae846d65ad0c0b4364e3279d8386162a103
pawelek <k.pawelek@vp.pl>
2024-09-11 17:38:57 +0200

    Update Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { FaArrowDown } from "react-icons/fa";
 import "../../styles/card.css";
@@ -7,15 +10,36 @@ import { Quotes } from "./Quotes";
 
 export const Hero: React.FC = () => {
   return (
-    <section className="p-28 h-screen bg-black  relative bg-hero-img bg-center bg-cover bg-fixed">
+    <motion.section
+      className="p-28 h-screen bg-black relative bg-hero-img bg-center bg-cover bg-fixed"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+    >
       <Quotes />
       <MarketingNavigation />
-      <div className="flex md:justify-between justify-center">
+      <motion.div
+        className="flex md:justify-between justify-center"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
         <div className="flex flex-col gap-8 justify-center items-center md:items-start">
-          <h1 className="text-neutral-100 text-3xl sm:text-6xl lg:text-8xl w-1/2 text-center md:text-start leading-loose font-sans">
+          <motion.h1
+            className="text-neutral-100 text-3xl sm:text-6xl lg:text-8xl w-1/2 text-center md:text-start leading-loose font-sans"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5, ease: "easeInOut" }}
+          >
             Create your way with Future!
-          </h1>
-          <div className="text-neutral-100 rounded-full w-96 shadow-lg backdrop-blur-lg shadow-sky-950">
+          </motion.h1>
+          <motion.div
+            className="text-neutral-100 rounded-full w-96 shadow-lg backdrop-blur-lg shadow-sky-950"
+            initial={{ opacity: 0, y: 30 }}
+            whileHover={{ scale: 1.05 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.2 }}
+          >
             <ul className="flex justify-between px-8 py-4">
               <li>
                 <button className="flex items-center gap-4">
@@ -34,10 +58,10 @@ export const Hero: React.FC = () => {
                 </Link>
               </li>
             </ul>
-          </div>
+          </motion.div>
         </div>
         <Cards />
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 };
