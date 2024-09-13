d1e41533a140705205f10c3333c1c947fd2c9367
pawelek <k.pawelek@vp.pl>
2024-09-13 15:18:00 +0200

    Update BooksGrid.tsx

--- app/components/home/BooksGrid.tsx
+++ app/components/home/BooksGrid.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/legacy/image";
 import React, { useState } from "react";
+import { FirstWord } from "../global/FirstWord";
 import { ThirdHeading } from "../global/Heading";
 import { Modal } from "../Modal";
 
@@ -34,27 +35,33 @@ export const BooksGrid: React.FC<BooksListProps> = ({ books }) => {
   };
   return (
     <>
-      <ul className="flex flex-wrap gap-6 mt-10 items-center justify-center">
-        {books.map((book) => (
-          <li
-            key={book._id}
-            className="cursor-pointer dark:shadow-sm"
-            onClick={() => handleBookClick(book)}
-          >
-            <div className="relative overflow-hidden rounded-md w-36 h-56">
-              <Image
-                src={book.cover}
-                alt={book.title}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-              />
-            </div>
-            <ThirdHeading>{book.title}</ThirdHeading>
-          </li>
-        ))}
-      </ul>
+      {books.length === 0 ? (
+        <div className="text-center mt-10">
+          <FirstWord>Ooops... Empty here.</FirstWord>
+        </div>
+      ) : (
+        <ul className="flex flex-wrap gap-6 mt-10 items-center justify-center">
+          {books.map((book) => (
+            <li
+              key={book._id}
+              className="cursor-pointer dark:shadow-sm"
+              onClick={() => handleBookClick(book)}
+            >
+              <div className="relative overflow-hidden rounded-md w-36 h-56">
+                <Image
+                  src={book.cover}
+                  alt={book.title}
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition="center"
+                  sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+                />
+              </div>
+              <ThirdHeading>{book.title}</ThirdHeading>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <Modal isOpen={isModalOpen} onClose={closeModal} book={selectedBook} />
     </>
