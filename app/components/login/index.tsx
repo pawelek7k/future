904d09a4ab75441fcec1ad1f41bd1d90b0bb2253
pawelek <k.pawelek@vp.pl>
2024-09-13 14:52:54 +0200

    add fixes

--- app/components/login/index.tsx
+++ app/components/login/index.tsx
@@ -2,8 +2,11 @@
 
 import { createUser } from "@/lib/signup/userApi";
 import { signIn } from "next-auth/react";
+import Notiflix from "notiflix";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { SecondaryButton } from "../global/Buttons";
+import { Loader } from "../global/Loader";
 import { MarketingNavigation } from "../navigations/MarketingNav";
 import { LoginForm } from "./Login";
 import { SignupForm } from "./Signup";
@@ -15,6 +18,9 @@ export const Container: React.FC = () => {
     username: "",
     password: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
 
   const toggleForm = () => {
     setIsLogin((prev) => !prev);
@@ -30,19 +36,26 @@ export const Container: React.FC = () => {
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
 
     if (isLogin) {
       try {
         const result = await signIn("credentials", {
-          redirect: true,
+          redirect: false,
           email: formData.email,
           password: formData.password,
-          callbackUrl: "/home",
         });
 
-        console.log("Sign In Result:", result);
+        if (result?.error) {
+          Notiflix.Notify.failure("Invalid email or password");
+        } else {
+          Notiflix.Notify.success("Logged in correctly");
+          router.push("/home");
+        }
       } catch (error) {
-        console.error("Login error:", error);
+        Notiflix.Notify.failure("Error logging in. Please try again.");
+      } finally {
+        setIsLoading(false);
       }
     } else {
       try {
@@ -52,14 +65,23 @@ export const Container: React.FC = () => {
           formData.password
         );
         console.log("User created:", result);
-        await signIn("credentials", {
-          redirect: true,
+
+        const signInResult = await signIn("credentials", {
+          redirect: false,
           email: formData.email,
           password: formData.password,
-          callbackUrl: "/home",
         });
+
+        if (signInResult?.error) {
+          Notiflix.Notify.failure("Error during login after registration.");
+        } else {
+          Notiflix.Notify.success("User registered and logged in successfully");
+          router.push("/home");
+        }
       } catch (error) {
-        console.error("Error creating user:", error);
+        Notiflix.Notify.failure("Error creating user. Try again later");
+      } finally {
+        setIsLoading(false);
       }
     }
   };
@@ -81,13 +103,14 @@ export const Container: React.FC = () => {
         />
       )}
       <div className="flex items-center gap-4 mt-10">
-        <p className="text-neutral-100">lub</p>
+        <p className="text-neutral-100">or</p>
       </div>
       <div className="mt-10">
         <SecondaryButton onClick={toggleForm}>
-          {isLogin ? "Switch to registrations" : "Switch to login"}
+          {isLogin ? "Switch to registration" : "Switch to login"}
         </SecondaryButton>
       </div>
+      {isLoading && <Loader />}
     </div>
   );
 };
