877d243566827618f3598dbe51c3ac96d0d8cd2e
pawelek <k.pawelek@vp.pl>
2024-10-13 12:05:13 +0200

    add fixes

--- app/components/landing/Features.tsx
+++ app/components/landing/Features.tsx
@@ -1,39 +1,65 @@
 "use client";
 
 import "@/app/styles/animations.css";
-import { features } from "@/lib/arrays/features";
-import { motion } from "framer-motion";
-import { Heading } from "./Heading";
 
 export const Features: React.FC = () => {
   return (
-    <section className="py-20 px-20 flex flex-col items-center gap-16">
-      <div className="text-center">
-        <Heading>How Future Works?</Heading>
-      </div>
-      <ul className="flex gap-12 flex-wrap items-center justify-center">
-        {features.map((feature) => (
-          <motion.li
-            className="p-4 sm:p-6 md:p-8 lg:p-12 rounded-lg bg-hero-bg shadow-lg w-full sm:w-80 md:w-96"
-            key={feature.id}
-            initial={{ opacity: 0, y: 30 }}
-            whileHover={{ scale: 1.05 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.2 }}
-          >
+    <section>
+      <span>Future</span>
+      <h2>Write, without the hassle</h2>
+      <ul>
+        <li>
+          <div>
+            <div>{/* img */}</div>
+
             <div>
-              <h3 className="text-neutral-200 font-semibold text-xl sm:text-2xl">
-                {feature.heading}
-              </h3>
-              <div className="flex items-center justify-center p-4 w-12 h-12 sm:w-18 sm:h-18">
-                {feature.icon}
-              </div>
-              <p className="text-neutral-300 text-sm sm:text-base">
-                {feature.p}
+              <span>1</span>
+            </div>
+            <div>
+              <h3>Sing up</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis, totam commodi odi
               </p>
             </div>
-          </motion.li>
-        ))}
+          </div>
+        </li>
+        <li>
+          <div>
+            <div>
+              <h3>Sing up</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis, totam commodi odi
+              </p>
+            </div>
+            <div>
+              <span>2</span>
+            </div>
+            <div>{/* img */}</div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div>{/* img */}</div>
+
+            <div>
+              <span>3</span>
+            </div>
+            <div>
+              <h3>Sing up</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis, totam commodi odi
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span>4</span>
+          </div>
+        </li>
       </ul>
     </section>
   );
--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -9,31 +9,12 @@ import { Quotes } from "./Quotes";
 export const Hero: React.FC = () => {
   return (
     <motion.section
-      className="p-28 h-screen bg-black relative bg-hero-img bg-center bg-cover bg-fixed"
+      className="p-28 h-screen bg-black relative bg-center bg-cover bg-fixed"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: "easeInOut" }}
     >
       <Quotes />
-      <MarketingNavigation />
-      <motion.div
-        className="flex md:justify-between justify-center"
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-      >
-        <div className="flex flex-col gap-8 justify-center items-center md:items-start">
-          <motion.h1
-            className="text-neutral-100 font-semibold text-3xl sm:text-6xl lg:text-8xl sm:w-1/2 text-center md:text-start leading-loose font-sans"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1.5, ease: "easeInOut" }}
-          >
-            Create your way with Future!
-          </motion.h1>
-        </div>
-        <Cards />
-      </motion.div>
     </motion.section>
   );
 };
