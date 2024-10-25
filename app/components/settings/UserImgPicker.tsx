7725649f9cbb2f88a4dff35bc9c147d18d031021
pawelek <k.pawelek@vp.pl>
2024-10-25 18:06:27 +0200

    Update UserImgPicker.tsx

--- app/components/settings/UserImgPicker.tsx
+++ app/components/settings/UserImgPicker.tsx
@@ -1,11 +1,7 @@
 "use client";
 import Image from "next/legacy/image";
-import { ChangeEvent, ReactNode, useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-
-interface Props {
-  children: ReactNode;
-}
 
 export const ImagePicker: React.FC = () => {
   const [pickedImage, setPickedImage] = useState<string | null>(null);
@@ -31,7 +27,7 @@ export const ImagePicker: React.FC = () => {
 
   return (
     <div className="z-10">
-      <div className="bg-white/20 rounded-full dark:bg-black/60 backdrop-blur-lg flex items-center gap-4 border border-sky-950 dark:border-rose-50">
+      <div className="bg-white/20 rounded-full dark:bg-black/60 backdrop-blur-lg flex items-center gap-4 border border-sky-950  w-28 h-28">
         <div
           className="w-28 h-28 rounded-full relative border border-sky-950 dark:border-rose-950 flex items-center justify-center font-sans cursor-pointer z-10"
           onClick={handleImageClick}
