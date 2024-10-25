71daeaf5a6682cb026faa3178032d5b85add7e65
pawelek <k.pawelek@vp.pl>
2024-10-25 18:17:33 +0200

    Update index.tsx

--- app/components/settings/index.tsx
+++ app/components/settings/index.tsx
@@ -1,6 +1,8 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { connectToDatabase } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { ImagePicker } from "./UserImgPicker";
 
 export const GlobalSettings: React.FC = async () => {
   const session = await getServerSession(authOptions);
@@ -9,5 +11,19 @@ export const GlobalSettings: React.FC = async () => {
     redirect("/login");
   }
 
-  return <></>;
+  let user;
+  try {
+    const db = await connectToDatabase();
+    const usersCollection = db.collection("users");
+    user = await usersCollection.findOne({ email: session.user?.email });
+  } catch (error) {
+    console.error("Error retrieving user data:", error);
+  }
+
+  return (
+    <div>
+      <ImagePicker />
+      <h2>{user?.username || user?.email.split("@")[0]}</h2>
+    </div>
+  );
 };
