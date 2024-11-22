c5025760d54a8bbed5aa5f354b6d0932f0681e7b
pawelek <k.pawelek@vp.pl>
2024-11-22 21:41:52 +0100

    .

--- app/[locale]/(marketing)/layout.tsx
+++ app/[locale]/(marketing)/layout.tsx
@@ -1,20 +1,15 @@
 import { Loader } from "@/app/components/global/Loader";
+import { inter } from "@/app/fonts/font";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Suspense } from "react";
-import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Future",
   description: "Your favorite writing app!",
 };
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
 
 export default async function RootLayout({
   children,
